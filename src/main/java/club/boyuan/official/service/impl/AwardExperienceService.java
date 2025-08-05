906e0ffea7397da8dd57598acb1ec8ea08b3bbde
Redmoon-2333 <3071187226@qq.com>
2025-08-05 21:24:08 +0800

    feat：实现全局搜索功能

--- src/main/java/club/boyuan/official/service/impl/AwardExperienceService.java
+++ src/main/java/club/boyuan/official/service/impl/AwardExperienceService.java
@@ -32,7 +32,6 @@ public class AwardExperienceService implements IAwardExperienceService {
     public List<AwardExperience> getByUserId(Integer userId) {
         return awardExperienceMapper.findByUserId(userId);
     }
-
     @Override
     public AwardExperience update(AwardExperience awardExperience) {
         // 检查获奖经历是否存在
