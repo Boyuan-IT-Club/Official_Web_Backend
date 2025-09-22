43e6986cb0564d6537ca5347f2880e58541216a6
Redmoon-2333 <3071187226@qq.com>
2025-09-22 17:41:54 +0800

    启用ObjectMapper的默认类型处理功能，确保对象能够正确地序列化和反序列化。

--- src/main/java/club/boyuan/official/config/RedisConfig.java
+++ src/main/java/club/boyuan/official/config/RedisConfig.java
@@ -22,10 +22,14 @@ public class RedisConfig {
         // 使用StringRedisSerializer序列化和反序列化key值
         template.setKeySerializer(new StringRedisSerializer());
         
-        // 配置ObjectMapper以支持Java 8时间类型
+        // 配置ObjectMapper以支持Java 8时间类型和类型信息
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 启用类型信息存储，以便正确反序列化对象
+        objectMapper.activateDefaultTyping(
+            objectMapper.getPolymorphicTypeValidator(),
+            ObjectMapper.DefaultTyping.NON_FINAL);
         
         // 使用GenericJackson2JsonRedisSerializer序列化和反序列化value值
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
